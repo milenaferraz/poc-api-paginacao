1c3597952f1af8ff7267172d983fc504ada72619
Milena Ferraz <59886457+milenaferraz@users.noreply.github.com>
2022-02-23 21:45:11 -0300

    first commit

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Paginacao.Data;
+using Paginacao.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Paginacao.Controllers
+{
+    [ApiController]
+    [Route("v1/todos")]
+    public class TodoController : ControllerBase
+    {
+        [HttpGet("load")]
+        public async Task<IActionResult> LoadAsync([FromServices] AppDbContext context)
+        {
+            for (var i = 0; i < 1348; i++)
+            {
+                var todo = new ToDo()
+                {
+                    Id = i + 1,
+                    Done = false,
+                    CreatedAt = DateTime.Now,
+                    Title = $"Tarefa {i}"
+                };
+                await context.Todos.AddAsync(todo);
+                await context.SaveChangesAsync();
+            }
+
+            return Ok();
+        }
+
+        [HttpGet("{skip:int}/{take:int}")]
+        public async Task<IActionResult> GetAsync([FromServices] AppDbContext context,
+            [FromRoute] int skip = 0,
+            [FromRoute] int take = 25)
+        {
+            var total = await context.Todos.CountAsync();
+            var todos = await context
+                .Todos
+                .AsNoTracking()
+                .Skip(skip)
+                .Take(take)
+                .ToListAsync();
+
+            return Ok(new
+            {
+                total,
+                data = todos
+            }
+            );
+        }
+    }
+}
---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Paginacao.Models;
+
+namespace Paginacao.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<ToDo> Todos { get; set; }
+
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder) => 
+            optionsBuilder.UseSqlite(connectionString: "DataSource=AppDbContext.db;Cache=Shared");
+    }
+}
